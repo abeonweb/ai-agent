5a0791c4351385e3cf056ad9f0fe5f9fd3f6cf49
abeonweb <abe@abecodes.com>
2025-06-27 15:31:04 +0100

    add tests for ai agent

---
+++ tests.py
@@ -0,0 +1,22 @@
+import unittest
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python_file import run_python_file
+
+
+class TestAgent(unittest.TestCase):
+    def test_agent(self):
+        print(run_python_file("calculator", "main.py"))
+        print("=========================================================================================")
+        print(run_python_file("calculator", "tests.py"))
+        print("=========================================================================================")
+        print(run_python_file("calculator", "../main.py"))
+        print("=========================================================================================")
+        print(run_python_file("calculator", "nonexistent.py"))
+        
+
+
+
+if __name__ == "__main__":
+    unittest.main()
